58499a8cd6fb652093ddc25f86b67f8943583414
tamar312 <82085022+tamar312@users.noreply.github.com>
2022-01-31 01:22:07 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import View from './compomens/view';
+import axios from 'axios'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { DetailsCard } from './compomens/detailsCard'
 
 function App() {
+
+  const [cardList, setCardList] = useState();
+
+  const onLoud = () => {
+    debugger
+    axios.get('angular_Response.json')
+      .then(res => {
+        setCardList(res.data);
+      })
+      .catch(error => alert('failed to load data error description: ' + error))
+  }
+  useEffect(onLoud, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" render={() => <View cardList={cardList} cardsView={cardList?.results} onLoud={onLoud} />} />
+        <Route exact path="/:detailsCard" render={() => <DetailsCard cardList={cardList} />} />
+      </Switch>
+    </Router>
   );
 }
 
